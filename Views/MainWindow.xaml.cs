65405d2babaa04adf30d7c0038d8dd8d40e8eb54
TheAltintas <Altintas12@outlook.dk>
2024-02-02 09:51:22 +0100

    Getting Movies

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TheMovies
+namespace TheMovies.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
---
+++ Views/MovieView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using TheMovies.ViewModels;
+
+namespace TheMovies.Views
+{
+    /// <summary>
+    /// Interaction logic for MovieView.xaml
+    /// </summary>
+    public partial class MovieView : Page
+    {
+        public MovieView()
+        {
+            InitializeComponent();
+            DataContext = new MovieViewModel();
+        }
+    }
+}
