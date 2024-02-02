9457aac614606263b2cc849655742de45e787c8c
TheAltintas <Altintas12@outlook.dk>
2024-02-02 11:21:28 +0100

    Read from CSV

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,21 +9,21 @@ namespace TheMovies.Models
     public class Movie
     {
         private string title;
-        private int runTime;
+        private string runTime;
         private List<string> genres;
         private string producer;
         private DateTime releaseDate;
         private DateTime showTime;
 
         public string Title {  get { return title; } set {  title = value; } }
-        public int RunTime { get {  return runTime; } set {  runTime = value; } }
+        public string RunTime { get {  return runTime; } set {  runTime = value; } }
         public List<string> Genres { get {  return genres; } set {  genres = value; } }
         public string Producer { get { return producer;} set { producer = value; } }
         public DateTime ReleaseDate { get {  return releaseDate; } set {  releaseDate = value; } }
         public DateTime ShowTime { get { return showTime; } set {  showTime = value; } }
 
 
-        public Movie(string title, int runTime, List<string> genres, string producer, DateTime releaseDate, DateTime showTime)
+        public Movie(string title, string runTime, List<string> genres, string producer, DateTime releaseDate, DateTime showTime)
         {
             Title = title;
             RunTime = runTime;
--- Models/MovieRepo.cs
+++ Models/MovieRepo.cs
@@ -1,16 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.IO;
 
 namespace TheMovies.Models
 {
     public class MovieRepo
     {
-        private ObservableCollection<Movie> _movies = new ObservableCollection<Movie>() { new Movie("General Wettendorff's evil teachings", 82, new List<string> { "Horror", "Comedy", "Romance", "Action" }, "Colonel Noerby", new DateTime(1986, 07, 01), DateTime.Now ) };
-        public bool CreateMovie(string title, int runtime, List<string> genres, string producer, DateTime releasedate, DateTime showtime)
+        private ObservableCollection<Movie> _movies = new ObservableCollection<Movie>() { new Movie("General Wettendorff's evil teachings", "01:12", new List<string> { "Horror", "Comedy", "Romance", "Action" }, "Colonel Noerby", new DateTime(1986, 07, 01), DateTime.Now) };
+
+        public MovieRepo()
+        {
+            using StreamReader sr = new StreamReader("C:\\Users\\Altin\\OneDrive\\Dokumenter\\The Movies2\\TheMovies\\Models\\Pr38_TheMovies.CSV");
+            _ = sr.ReadLine();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] lines = line.Split(';');
+                List<string> genres = new List<string>();
+                foreach (string genre in lines[4].Split(','))
+                {
+                    genres.Add(genre);
+                }
+                Movie movie = new(lines[3], lines[5], genres, lines[6], DateTime.Parse(lines[7]), DateTime.Parse(lines[2]));
+                _movies.Add(movie);
+            }
+        }
+        public bool CreateMovie(string title, string runtime, List<string> genres, string producer, DateTime releasedate, DateTime showtime)
         {
             Movie newMovie = new(title, runtime, genres, producer, releasedate, showtime);
             _movies.Add(newMovie);
@@ -25,6 +39,8 @@ namespace TheMovies.Models
                 return false;
             }
         }
+
+
 
         public ObservableCollection<Movie> GetMovies() { return _movies; }
 
